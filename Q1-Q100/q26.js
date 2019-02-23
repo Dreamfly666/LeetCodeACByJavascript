a23594b783a6d3839733c11e692db1a8fa8baa47
lishanpeng <18562011169@163.com>
2019-02-24 00:48:14 +0800

    format q26

--- Q1-Q100/q26.js
+++ Q1-Q100/q26.js
@@ -5,20 +5,21 @@
  * @param {number[]} nums
  * @param {number}
  */
-var removeDuplicates = function(nums){
-    if(nums.length === 0) return nums.length
+var removeDuplicates = function (nums) {
+    if (nums.length === 0) return nums.length
     let cur = nums[0]
     let index = 1
-    while(index < nums.length){
-        if(cur != nums[index]){
+    while (index < nums.length) {
+        if (cur != nums[index]) {
             cur = nums[index]
             index++
         } else {
-            nums.splice(index,1)
+            nums.splice(index, 1)
         }
     }
     return nums.length
 }
 
-let arr =[0,0,1,1,2]
+// runtime 104ms(beats 49.3%)  memory  37.5m(beats 16.9%)
+let arr = [0, 0, 1, 1, 2]
 console.log(removeDuplicates(arr))
