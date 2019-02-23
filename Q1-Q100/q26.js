52754570bfbc938074cd332f837a226c7b9c3d25
lishanpeng <18562011169@163.com>
2019-02-24 00:34:31 +0800

    q26

---
+++ Q1-Q100/q26.js
@@ -0,0 +1,24 @@
+// 26.Remove Duplicate from Sorted Array 
+// do not allocate extra space for another array,you must do this by modifying the 
+// the input arrary with O(1) extra memory
+/**
+ * @param {number[]} nums
+ * @param {number}
+ */
+var removeDuplicates = function(nums){
+    if(nums.length === 0) return nums.length
+    let cur = nums[0]
+    let index = 1
+    while(index < nums.length){
+        if(cur != nums[index]){
+            cur = nums[index]
+            index++
+        } else {
+            nums.splice(index,1)
+        }
+    }
+    return nums.length
+}
+
+let arr =[0,0,1,1,2]
+console.log(removeDuplicates(arr))
