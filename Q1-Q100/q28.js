d8bd23abaa33fd24690d022259b0f30285cd796e
lishanpeng <18562011169@163.com>
2019-03-05 17:03:07 +0800

    q28 implement strStr

--- Q1-Q100/q28.js
+++ Q1-Q100/q28.js
@@ -1,9 +1,27 @@
 // 28.Implement strStr()
 
-var strStr = function(haystack,needle){
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function (haystack, needle) {
     // if(!needle) return 0
-    return haystack.indexOf(needle)
+    const slen = haystack.length
+    const plen = needle.length
+    if (slen == plen) return haystack == needle ? 0 : -1
+    // <= notice
+    for (let i = 0; i <= slen - plen; i++) {
+        let j
+        for (j = 0; j < plen; j++) {
+            if (haystack[i + j] !== needle[j]) {
+                break
+            }
+        }
+        if (j == plen) return i
+    }
+    return -1
 }
-// runtime:56ms(beats 77.4%) memory 33.7m(beats84.96%)
+// runtime:64ms(beats 55.49%) memory 33.8m(beats66.37%)
 
-console.log(strStr('hello',''))
+console.log(strStr('mississippi', 'pi'))
