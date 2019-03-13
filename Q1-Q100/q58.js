d98670ae94d09b42c150cccfb6df3d3cebbac9c9
lishanpeng <18562011169@163.com>
2019-03-13 23:44:16 +0800

    update q58

--- Q1-Q100/q58.js
+++ Q1-Q100/q58.js
@@ -1,5 +1,8 @@
 // 58.Length of Last Word
 
 var lengthOfLastWord = function(s){
-    let arr = s.split('')
+   let arr =  s.trim().split(' ')
+   return arr[arr.length-1].length
 }
+// runtime 56ms beats 78.16% memory 33.7m beats 66.94%
+let re = lengthOfLastWord('hello world')
