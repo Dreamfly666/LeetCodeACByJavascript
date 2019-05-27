e5adf771be0579c89550270a8abc16556f0a8d1c
lishanpeng <18562011169@163.com>
2019-05-27 14:00:13 +0800

    q682

---
+++ Q300+/q682.js
@@ -0,0 +1,32 @@
+// 682.Baseball Game
+
+var calPoints = function (ops) {
+    let result = []
+    let pr1, pr2
+    ops.forEach(item => {
+        switch (item) {
+            case "C":
+                if (result.length) {
+                    result.pop()
+                }
+                break
+            case "D":
+                pr1 = result.pop()
+                result.push(pr1, pr1 * 2)
+                break
+            case "+":
+                pr1 = result.pop()
+                pr2 = result.pop()
+                result.push(pr2, pr1, pr2 + pr1)
+                break
+            default:
+                result.push(item * 1)
+        }
+    })
+    return result.reduce((total, num) => {
+        return total + num
+    })
+}
+
+let arr = ["5", "-2", "4", "C", "D", "9", "+", "+"]
+console.log(calPoints(arr))
